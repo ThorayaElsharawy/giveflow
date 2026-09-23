710dd63ec25693770c42e6bdfff4c2865d0eee23
ThorayaElsharawy <thorayaelsharawy@gmail.com>
2026-09-23 11:46:00 +0300

    feat: complete home page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {Text, View, Image, FlatList} from "react-native";
+import {Text, View, Image, FlatList, TouchableOpacity, ScrollView} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import {formatCurrency} from "@/lib/utils";
@@ -42,39 +42,99 @@ export default function App() {
         }
     ]
     return (
-        <SafeAreaView className="flex-1 bg-[#f3f8fd] p-5">
-            <View className='px-4 py-3 flex-row items-center gap-4'>
-                <View className='rounded-full p-3 bg-[#3b82f6]'>
-                    <Ionicons name='person-outline' size={20} color='#ffffff'/>
+        <ScrollView className="flex-1 bg-[#f3f8fd] p-5">
+            <SafeAreaView>
+                <View className='px-4 py-3 flex-row items-center gap-4'>
+                    <View className='rounded-full p-3 bg-[#3b82f6]'>
+                        <Ionicons name='person-outline' size={20} color='#ffffff'/>
+                    </View>
+                    <Text className='text-xl font-roboto-bold'>Thoraya Adel</Text>
                 </View>
-                <Text className='text-xl font-roboto-bold'>Thoraya Adel</Text>
-            </View>
 
-            <View className='bg-[#3b82f6] p-4 rounded-tl-[32px] rounded-br-[32px] mt-2'>
-                <Text className='font-bold text-2xl text-white font-roboto-bold'>Balance</Text>
-                <Text className='text-5xl text-white mt-10 font-roboto-regular'>
-                    {formatCurrency(248948)}</Text>
-            </View>
-
-            <View>
-                <ListHeading title='Donation Ways'/>
-
-                <FlatList className='mt-4' data={donationWays} renderItem={({item}) => (
-                    <DonationCards item={item}/>)}
-                          keyExtractor={item => item.id}
-                          horizontal={true}
-                          showsHorizontalScrollIndicator={false}
-                          ListEmptyComponent={<Text>No Donation Ways List</Text>}
-                />
-            </View>
-
-            <View>
-                <ListHeading title='Donation Numbers'/>
-
-                <View className='bg-red-200 p-3 mt-4'>
-                    <Text>Donors number</Text>
+                <View className='bg-[#3b82f6] p-4 rounded-tl-[32px] rounded-br-[32px] mt-2'>
+                    <Text className='font-bold text-2xl text-white font-roboto-bold mt-2'>Balance</Text>
+                    <Text className='text-5xl text-white mt-6 font-roboto-regular'>
+                        {formatCurrency(248948)}</Text>
                 </View>
-            </View>
-        </SafeAreaView>
+
+                <View>
+                    <ListHeading title='Donation Ways'/>
+
+                    <FlatList className='mt-4' data={donationWays} renderItem={({item}) => (
+                        <DonationCards item={item}/>)}
+                              keyExtractor={item => item.id}
+                              horizontal={true}
+                              showsHorizontalScrollIndicator={false}
+                              ListEmptyComponent={<Text>No Donation Ways List</Text>}
+                    />
+                </View>
+
+                <View>
+                    <ListHeading title='Donation Numbers'/>
+
+                    <View className='bg-red-300 rounded-t-[32px]  p-5 mt-4'>
+                        <View className='flex-row items-center justify-between'>
+                            <View className='flex-row items-center gap-2'>
+                                <Image source={require('../../../assets/images/dontion-icon.png')} width={30}
+                                       height={30} className="w-12 h-12"/>
+                                <View>
+                                    <Text className='text-2xl'>Donor Numbers</Text>
+                                    <Text className='text-gray-600'>2500k</Text>
+                                </View>
+                            </View>
+                            <View>
+                                <Text className='text-2xl'>{formatCurrency(2522)}</Text>
+                                <Text className='text-gray-600'>per month</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View className='bg-[#f8d54e] p-4 px-5 mt-4'>
+                        <View className='flex-row items-center justify-between'>
+                            <View className='flex-row items-center gap-2'>
+                                <Image source={require('../../../assets/images/dontion-icon.png')} width={30}
+                                       height={30} className="w-12 h-12"/>
+                                <View>
+                                    <Text className='text-2xl'>Donor Numbers</Text>
+                                    <Text className='text-gray-600'>2500k</Text>
+                                </View>
+                            </View>
+                            <View>
+                                <Text className='text-2xl'>{formatCurrency(2522)}</Text>
+                                <Text className='text-gray-600'>per month</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View className='bg-[#acd8c4] rounded-b-[32px] p-4 px-5 mt-4'>
+                        <View className='flex-row items-center justify-between'>
+                            <View className='flex-row items-center gap-2'>
+                                <Image source={require('../../../assets/images/dontion-icon.png')} width={30}
+                                       height={30} className="w-12 h-12"/>
+
+                                <View>
+                                    <Text className='text-2xl'>Donor Numbers</Text>
+                                    <Text className='text-gray-600'>2500k</Text>
+                                </View>
+                            </View>
+                            <View>
+                                <Text className='text-2xl'>{formatCurrency(2522)}</Text>
+                                <Text className='text-gray-600'>per month</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+
+                <View>
+                    <TouchableOpacity className='rounded-sm border border-[#3b82f6] p-3 mt-6'>
+                        <Text className='text-center text-[#3b82f6] p-2 font-bold text-lg'>Exising Donor</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity className='rounded-sm bg-[#3b82f6] p-3 mt-6'>
+                        <Text className='text-center text-white p-2 font-bold text-lg'>New Donor</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </ScrollView>
     );
 }
